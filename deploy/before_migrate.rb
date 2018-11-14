0769df2954aac8563be76ab9ef73d4c8842a3db7
Shintaro Tanaka <tallow@dirt.ninja>
2018-11-14 18:13:50 +0900

    Temporally removed assets:precompile task before migration

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,10 +1,10 @@
-Chef::Log.info "Running deploy/before_migrate.rb"
-
-env = node[:deploy][:rails_sample][:rails_env]
-current_release = release_path
-
-execute "rake assets:precompile" do
-  cwd current_release
-  command "bundle exec rake assets:precompile"
-  environment "RAILS_ENV" => env
-end
+#Chef::Log.info "Running deploy/before_migrate.rb"
+#
+#env = node[:deploy][:rails_sample][:rails_env]
+#current_release = release_path
+#
+#execute "rake assets:precompile" do
+#  cwd current_release
+#  command "bundle exec rake assets:precompile"
+#  environment "RAILS_ENV" => env
+#end
